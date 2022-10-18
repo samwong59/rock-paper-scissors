4cad9968aa75fa64ec4739f91b1df093e345f624
Sam <samwong59@outlook.com>
2022-10-18 13:45:57 +0100

    Create playFiveGameSet()
    Puts user in a five round game versus the computer and reports the
    overall winner at the end

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const INVALID_INPUT_MESSAGE = "Invalid input, try again!";
+
+let winCount = 0;
+let lossCount = 0;
+
+playFiveGameSet();
+
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomChoice = getRandomInt();
@@ -12,31 +19,63 @@ function playGame(playerSelection, computerSelection) {
   playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
   console.log(`Player chose ${playerSelection} vs ${computerSelection}`);
   if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
-    return "Invalid input, try again!";
+    return INVALID_INPUT_MESSAGE;
   }
   if (playerSelection == computerSelection) {
     return "It's a draw!";
   }
   else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    winCount++;
     return "You won!";
   }
   else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    lossCount++;
     return "You lost, sorry.";
   }
   else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    lossCount++;
     return "You lost, sorry.";
   }
   else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    winCount++;
     return "You won!";
   }
   else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    lossCount++;
     return "You lost, sorry.";
   }
   else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    winCount++;
     return "You won!";
   }
 }
 
-let userChoice = prompt("Rock, Paper or Scissors?");
-let gameResult = playGame(userChoice, getComputerChoice());
-console.log(gameResult);
+function playFiveGameSet() {
+  let keepGoing = true;
+  let gameCount = 0;
+
+  while (keepGoing) {
+    let userChoice = prompt("Rock, Paper or Scissors?");
+    let gameResult = playGame(userChoice, getComputerChoice());
+    if (gameResult == INVALID_INPUT_MESSAGE) {
+      console.log(INVALID_INPUT_MESSAGE);
+      continue;
+    }
+    console.log(gameResult);
+    gameCount++;
+    if (gameCount == 5) {
+      if (winCount > lossCount) {
+        console.log(`You won ${winCount} to ${lossCount}!`);
+      }
+      else if (winCount < lossCount) {
+        console.log(`You lost ${winCount} to ${lossCount}.`);
+      }
+      else {
+        console.log("It's an overall draw!");
+      }
+      winCount = 0;
+      lossCount = 0;
+      keepGoing = false;
+    }
+  }
+}
