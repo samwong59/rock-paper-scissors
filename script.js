1fccbdc91cae75cf7102e85778d24ac14af95f7d
Sam <samwong59@outlook.com>
2022-10-18 13:21:40 +0100

    Add playGame(playerSelection, computerSelection)
    Takes users choice and computers choice and returns the result of the
    match so that we can determine who wins and who lost;

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   const randomChoice = getRandomInt();
-  console.log(randomChoice);
   return choices[randomChoice];
 }
 
@@ -9,5 +8,35 @@ function getRandomInt() {
   return Math.floor(Math.random() * 3);
 }
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
+function playGame(playerSelection, computerSelection) {
+  playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+  console.log(`Player chose ${playerSelection} vs ${computerSelection}`);
+  if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
+    return "Invalid input, try again!";
+  }
+  if (playerSelection == computerSelection) {
+    return "It's a draw!";
+  }
+  else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    return "You won!";
+  }
+  else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    return "You lost, sorry.";
+  }
+  else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    return "You lost, sorry.";
+  }
+  else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    return "You won!";
+  }
+  else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    return "You lost, sorry.";
+  }
+  else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    return "You won!";
+  }
+}
+
+let userChoice = prompt("Rock, Paper or Scissors?");
+let gameResult = playGame(userChoice, getComputerChoice());
+console.log(gameResult);
