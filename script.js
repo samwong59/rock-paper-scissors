425de20a5c387d70c9c7581c1d5caeb9141c138d
Sam <samwong59@outlook.com>
2022-12-02 18:07:20 +0000

    Display current score and overall winner

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ let lossCount = 0;
 const rockButton = document.querySelector("#rockButton");
 const paperButton = document.querySelector("#paperButton");
 const scissorsButton = document.querySelector("#scissorsButton");
-const results = document.querySelector("#results");
+const result = document.querySelector("#result");
+const score = document.querySelector("#score");
+const fiveGameResult = document.querySelector("#fiveGameResult");
 
 rockButton.addEventListener("click", playGame);
 paperButton.addEventListener("click", playGame);
@@ -23,36 +25,49 @@ function getRandomInt() {
 }
 
 function playGame(e) {
+  fiveGameResult.textContent = "";
+
   const eventButtonTextContent = e.target.textContent
   let playerSelection = (eventButtonTextContent) == "Rock" ? "Rock" : 
     (eventButtonTextContent == "Paper") ? "Paper" : "Scissors";
   const computerSelection = getComputerChoice();
   console.log(`Player chose ${playerSelection} vs ${computerSelection}`);
   if (playerSelection == computerSelection) {
-    results.textContent = "It's a draw!";
+    result.textContent = "It's a draw!";
   }
   else if (playerSelection == "Paper" && computerSelection == "Rock") {
     winCount++;
-    results.textContent = "You won!";
+    result.textContent = "You won!";
   }
   else if (playerSelection == "Paper" && computerSelection == "Scissors") {
     lossCount++;
-    results.textContent = "You lost.";
+    result.textContent = "You lost.";
   }
   else if (playerSelection == "Scissors" && computerSelection == "Rock") {
     lossCount++;
-    results.textContent = "You lost.";
+    result.textContent = "You lost.";
   }
   else if (playerSelection == "Scissors" && computerSelection == "Paper") {
     winCount++;
-    results.textContent = "You won!";
+    result.textContent = "You won!";
   }
   else if (playerSelection == "Rock" && computerSelection == "Paper") {
     lossCount++;
-    results.textContent = "You lost.";
+    result.textContent = "You lost.";
   }
   else if (playerSelection == "Rock" && computerSelection == "Scissors") {
     winCount++;
-    results.textContent = "You won!";
+    result.textContent = "You won!";
+  }
+  score.textContent = `${winCount} - ${lossCount}`;
+  if (winCount == 5) {
+    fiveGameResult.textContent = "You won the overall game!";
+    lossCount = 0;
+    winCount = 0;
+  }
+  if (lossCount == 5) {
+    fiveGameResult.textContent = "The computer won the overall game. Try again.";
+    lossCount = 0;
+    winCount = 0;
   }
 }
