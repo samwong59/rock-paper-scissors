5ebf17ac514771bde821a4a58b19ddfb0fcf7069
Sam <samwong59@outlook.com>
2022-12-02 17:42:42 +0000

    Make player choice buttons that play game on click
    Added three buttons that represent 'Rock', 'Paper' and 'Scissors'. On
    click will play a game with the selected player choice.

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const INVALID_INPUT_MESSAGE = "Invalid input, try again!";
 let winCount = 0;
 let lossCount = 0;
 
-playFiveGameSet();
+const rockButton = document.querySelector("#rockButton");
+const paperButton = document.querySelector("#paperButton");
+const scissorsButton = document.querySelector("#scissorsButton");
+
+rockButton.addEventListener("click", playGame);
+paperButton.addEventListener("click", playGame);
+scissorsButton.addEventListener("click", playGame);
 
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -15,67 +21,40 @@ function getRandomInt() {
   return Math.floor(Math.random() * 3);
 }
 
-function playGame(playerSelection, computerSelection) {
-  playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+function playGame(e) {
+  const eventButtonTextContent = e.target.textContent
+  let playerSelection = (eventButtonTextContent) == "Rock" ? "Rock" : 
+    (eventButtonTextContent == "Paper") ? "Paper" : "Scissors";
+  const computerSelection = getComputerChoice();
   console.log(`Player chose ${playerSelection} vs ${computerSelection}`);
   if (!(playerSelection == "Rock" || playerSelection == "Paper" || playerSelection == "Scissors")) {
-    return INVALID_INPUT_MESSAGE;
+    console.log("INVALID_INPUT_MESSAGE");
   }
   if (playerSelection == computerSelection) {
-    return "It's a draw!";
+    console.log("It's a draw!");
   }
   else if (playerSelection == "Paper" && computerSelection == "Rock") {
     winCount++;
-    return "You won!";
+    console.log("You won!");
   }
   else if (playerSelection == "Paper" && computerSelection == "Scissors") {
     lossCount++;
-    return "You lost, sorry.";
+    console.log("You lost, sorry.");
   }
   else if (playerSelection == "Scissors" && computerSelection == "Rock") {
     lossCount++;
-    return "You lost, sorry.";
+    console.log("You lost, sorry.");
   }
   else if (playerSelection == "Scissors" && computerSelection == "Paper") {
     winCount++;
-    return "You won!";
+    console.log("You won!");
   }
   else if (playerSelection == "Rock" && computerSelection == "Paper") {
     lossCount++;
-    return "You lost, sorry.";
+    console.log("You lost, sorry.");
   }
   else if (playerSelection == "Rock" && computerSelection == "Scissors") {
     winCount++;
-    return "You won!";
-  }
-}
-
-function playFiveGameSet() {
-  let keepGoing = true;
-  let gameCount = 0;
-
-  while (keepGoing) {
-    let userChoice = prompt("Rock, Paper or Scissors?");
-    let gameResult = playGame(userChoice, getComputerChoice());
-    if (gameResult == INVALID_INPUT_MESSAGE) {
-      console.log(INVALID_INPUT_MESSAGE);
-      continue;
-    }
-    console.log(gameResult);
-    gameCount++;
-    if (gameCount == 5) {
-      if (winCount > lossCount) {
-        console.log(`You won ${winCount} to ${lossCount}!`);
-      }
-      else if (winCount < lossCount) {
-        console.log(`You lost ${winCount} to ${lossCount}.`);
-      }
-      else {
-        console.log("It's an overall draw!");
-      }
-      winCount = 0;
-      lossCount = 0;
-      keepGoing = false;
-    }
+    console.log("You won!");
   }
 }
